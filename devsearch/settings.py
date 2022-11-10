9dcacf6d237b8590cca9a387a0816f9e0bfd28e2
Rok Sprogar <rok.sprogar@gmail.com>
2022-11-10 21:56:16 +0100

    add a simple crud and the template

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%fm+_(v=dl6oi59o@+91^&!w+(vhoq%j_+ry%wxg*dzvjg*j+t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name="projects"),
+    path('projects/create/', views.createProject, name="project_create"),
+    path('projects/update/<str:pk>/', views.updateProject, name="project_update"),
+    path('projects/delete/<str:pk>/', views.deleteProject, name="project_delete"),
     path('projects/<str:pk>/', views.project, name="project"),
 ]
