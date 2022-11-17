8fb9565a8fe85a130d6300862b67f1f09652429d
Rok Sprogar <rok.sprogar@gmail.com>
2022-11-17 23:46:02 +0100

    code cleanup

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Project
+from .models import Project, Tag
 from .forms import ProjectForm, ReviewForm
 from .utils import searchProjects, paginateProjects
 
@@ -40,11 +40,18 @@ def createProject(request):
     form = ProjectForm()
     
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', ' ').split()
         form = ProjectForm(request.POST, request.FILES)
+        
         if form.is_valid():
             project = form.save(commit=False)
             project.owner = profile
             project.save()
+            
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+            
             return redirect('account')
 
     context = {'form': form}
@@ -57,12 +64,19 @@ def updateProject(request, pk):
     form = ProjectForm(instance=project)
     
     if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',', ' ').split()
         form = ProjectForm(request.POST, request.FILES, instance=project)
+        
         if form.is_valid():
-            form.save()
+            project = form.save()
+
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+            
             return redirect('account')
 
-    context = {'form': form}
+    context = {'form': form, 'project': project}
     return render(request, 'projects/project-form.html', context)
 
 @login_required(login_url="login")
