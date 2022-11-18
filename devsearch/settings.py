792745f37e99d0d5c7a396a9f0301fb0034ed097
Rok Sprogar <30501923+roksprogar@users.noreply.github.com>
2022-11-18 21:42:20 +0100

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -129,10 +129,10 @@ USE_I18N = True
 USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_PORT = 2525
-EMAIL_HOST_USER = '032b7e77484729'
-EMAIL_HOST_PASSWORD = 'c6927f537a8460'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
